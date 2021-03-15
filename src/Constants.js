4a6df60ef53ea2b0cf90cb04312a4ffc471d0e51
Windows <massiopia@gmail.com>
2021-03-15 01:54:23 -0700

    channel types

--- src/Constants.js
+++ src/Constants.js
@@ -21,6 +21,16 @@ module.exports.GATEWAY_OP_CODES = {
   HEARTBEAT_ACK: 11,
 };
 
+module.exports.CHANNEL_TYPES = {
+  0: "GUILD_TEXT",
+  1: "DM",
+  2: "GUILD_VOICE",
+  3: "GROUP_DM",
+  4: "GUILD_CATEGORY",
+  5: "GUILD_NEWS",
+  6: "GUILD_STORE"
+}
+
 module.exports.INTENTS = {
   GUILDS: 1 << 0,
   GUILD_MEMBERS: 1 << 1,
